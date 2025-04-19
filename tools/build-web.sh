080019c5423030d10031e8db6fcdb225cb6d005a
Ben Coleman <benc.uk@gmail.com>
2025-04-19 18:17:25 +0100

    chmod blues

--- tools/build-web.sh
+++ tools/build-web.sh
@@ -3,7 +3,7 @@
 echo "🔨 Building web version..."
 npx love.js ./src/ ./dist/web -c -t "Loading..."
 
-echo "📚 Copying files..."
+echo "📚 Copying custom files..."
 cp ./dist/love.css ./dist/web/theme/love.css
 cp ./dist/favicon.ico ./dist/web/
 cp ./dist/bg.png ./dist/web/theme/bg.png
